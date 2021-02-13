3ecbed43371aaa599a7d736ef22f8725b802fb06
AarthiArul <aarthimarisarul@gmail.com>
2021-02-13 22:37:30 +0530

    completed

---
+++ src/org/tes/Data.java
@@ -0,0 +1,14 @@
+package org.tes;
+
+public class Data {
+	private void printSome()
+	{
+		System.out.println("Data Print");
+	}
+	public static void main(String[] args) {
+		
+		Data d=new Data();
+		d.printSome();
+	}
+
+}
